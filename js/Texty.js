bc7061b4f7e57992928bc3840dede6b20f8607c2
Wunkolo <Wunkolo@gmail.com>
2018-01-30 23:12:41 -0800

    Add superscript

--- js/Texty.js
+++ js/Texty.js
@@ -59,6 +59,89 @@ var Transforms = [
 					return String.fromCodePoint(CharCode + 0x1D79E);
 				});
 		}
+	},
+	// Add other math-unicodes here later
+	{
+		name: "SuperScript",
+		id: "superscript",
+		description: "Super script lettering",
+		Apply: function (Input) {
+			var Table = {
+				"0": "\u2070",
+				"1": "\u00B9",
+				"2": "\u00B2",
+				"3": "\u00B3",
+				"4": "\u2074",
+				"5": "\u2075",
+				"6": "\u2076",
+				"7": "\u2077",
+				"8": "\u2078",
+				"9": "\u2079",
+				"a": "\u1D43",
+				"b": "\u1D47",
+				"c": "\u1D9C",
+				"d": "\u1D48",
+				"e": "\u1D49",
+				"f": "\u1DA0",
+				"g": "\u1D4D",
+				"h": "\u02B0",
+				"i": "\u1DA6",
+				"j": "\u02B2",
+				"k": "\u1D4F",
+				"l": "\u02E1",
+				"m": "\u1D50",
+				"n": "\u207F",
+				"o": "\u1D52",
+				"p": "\u1D56",
+				//"q": "q",
+				"r": "\u02B3",
+				"s": "\u02E2",
+				"t": "\u1D57",
+				"u": "\u1D58",
+				"v": "\u1D5B",
+				"w": "\u02B7",
+				"x": "\u02E3",
+				"y": "\u02B8",
+				"z": "\u1DBB",
+				"A": "\u1D2C",
+				"B": "\u1D2E",
+				"C": "\u1D9C",
+				"D": "\u1D30",
+				"E": "\u1D31",
+				"F": "\u1DA0",
+				"G": "\u1D33",
+				"H": "\u1D34",
+				"I": "\u1D35",
+				"J": "\u1D36",
+				"K": "\u1D37",
+				"L": "\u1D38",
+				"M": "\u1D39",
+				"N": "\u1D3A",
+				"O": "\u1D3C",
+				"P": "\u1D3E",
+				//"Q": "Q",
+				"R": "\u1D3F",
+				"S": "\u02E2",
+				"T": "\u1D40",
+				"U": "\u1D41",
+				"V": "\u2C7D",
+				"W": "\u1D42",
+				"X": "\u02E3",
+				"Y": "\u02B8",
+				"Z": "\u1DBB",
+				"\\+": "\u207A",
+				"-": "\u207B",
+				"=": "\u207C",
+				"\\(": "\u207D",
+				"\\)": "\u207E"
+			};
+			return Input.replace(
+				new RegExp(Object.keys(Table).join("|"), "gu"),
+				function (CurChar) {
+					return Table[CurChar]
+				}
+			);
+		}
 	}
 ];
 
