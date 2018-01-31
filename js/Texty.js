fecc53c0856196a82c64f0aa3e8a7fa794cc1e3b
Wunkolo <Wunkolo@gmail.com>
2018-01-30 22:15:41 -0800

    Add bubbletext

--- js/Texty.js
+++ js/Texty.js
@@ -11,6 +11,31 @@ var Transforms = [
 				return String.fromCharCode(CharCode + 0xFEE0);
 			});
 		}
+	},
+	{
+		name: "Bubble",
+		id: "bubble",
+		description: "Bubble lettering",
+		Apply: function (Input) {
+			return Input
+			// A-Z
+			.replace(/[A-Z]/gu, function (CurChar) {
+				var CharCode = CurChar.charCodeAt(0);
+				return String.fromCharCode(CharCode + 0x2475);
+			})
+			// a-z
+			.replace(/[a-z]/gu, function (CurChar) {
+				var CharCode = CurChar.charCodeAt(0);
+				return String.fromCharCode(CharCode + 0x246F);
+			})
+			// 1-9
+			.replace(/[1-9]/gu, function (CurChar) {
+				var CharCode = CurChar.charCodeAt(0);
+				return String.fromCharCode(CharCode + 0x242F);
+			})
+			// 0
+			.replace(/0/gu, "\u24EA");
+		}
 	}
 ];
 
